59ab73fab34b027c4c0e740bf57ff98915801416
Jia <jiayoo.dev@gmail.com>
2024-07-17 15:31:17 +0900

    qna리스트 헤더 변경

--- veiwServer/src/main/java/com/example/veiwServer/controller/hospital/HospitalQnAListController_jia.java
+++ veiwServer/src/main/java/com/example/veiwServer/controller/hospital/HospitalQnAListController_jia.java
@@ -22,7 +22,7 @@ public class HospitalQnAListController_jia {
 	
 	@GetMapping("/qna")
 	public List<Support> getMyQnAList(HttpServletRequest request) {
-		Long userId = Long.parseLong(request.getHeader("username"));
+		Long userId = Long.parseLong(request.getHeader("MemberId"));
 		return supportRepo.findAllByMemberId(userId);
 	}
 }
