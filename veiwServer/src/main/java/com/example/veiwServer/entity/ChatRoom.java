d8292590700a786eac5cdca7fb6e202612ff997b
Jia <jiayoo.dev@gmail.com>
2024-07-23 10:51:19 +0900

    챗룸 리포 업데이트

--- veiwServer/src/main/java/com/example/veiwServer/entity/ChatRoom.java
+++ veiwServer/src/main/java/com/example/veiwServer/entity/ChatRoom.java
@@ -1,6 +1,12 @@
 package com.example.veiwServer.entity;
 
-import java.util.Set;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.veiwServer.dto.IChatRoomListDto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +14,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- veiwServer/src/main/java/com/example/veiwServer/repository/ChatRoomRepository.java
+++ veiwServer/src/main/java/com/example/veiwServer/repository/ChatRoomRepository.java
@@ -4,26 +4,35 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.example.veiwServer.entity.Chat;
 import com.example.veiwServer.entity.ChatRoom;
 import com.example.veiwServer.entity.Member;
 
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>{
+	
 	public ChatRoom findByUserAndHospital(Member user, Member hospital);
+	
+	public ChatRoom findByHospitalAndUser(Member hospital, Member user);
 	
 	public List<ChatRoom> findByUser(Member user);
     
 	public List<ChatRoom> findByHospital(Member hospital);
 	
+	@Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :chatRoomId ORDER BY c.sendDate DESC")
+    List<Chat> findChatsByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 	
 	
-	@Query(value = "SELECT c.*, cr.* " +
-            "FROM chat c " +
-            "JOIN chat_room cr ON c.chat_room_id = cr.id " +
-            "WHERE cr.user_id = :userId " +
-            "ORDER BY c.send_date DESC", nativeQuery = true)
-	List<Object[]> findChatsAndRoomsByUserId(Long userId);
-
+	//@Query("SELECT cr FROM ChatRoom cr WHERE (cr.member1 = :member1 AND cr.member2 = :member2) OR (cr.member1 = :member2 AND cr.member2 = :member1)")
+    //ChatRoom findByMembers(@Param("member1") Member member1, @Param("member2") Member member2);
+	
+	
+	
+	@Query("SELECT cr FROM ChatRoom cr WHERE (cr.user = :member1 AND cr.hospital = :member2) OR (cr.user = :member2 AND cr.hospital = :member1)")
+    ChatRoom findByMembers(@Param("member1") Member member1, @Param("member2") Member member2);
+	
 	
 	
 }
